4acc8d0180fee4b1c997a36d2ccbbc9d75458c5f
NkwamoMaeva <nkwamomaeva@gmail.com>
2023-06-23 17:17:28 +0200

    Project base

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <header>
+      <v-toolbar class="white" color="white" density="comfortable">
+        <v-toolbar-title>
+          <div class="d-flex flex-row align-center">
+            <v-icon size="large" class="mx-2" color="red-darken-1">mdi-pokeball</v-icon>
+            <strong> <strong class="text-red-darken-1">Poke</strong>Dex </strong>
+          </div>
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon color="red-darken-1">mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </header>
+    <v-main class="bg-grey-lighten-5 pt-8">
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  })
+}
+</script>
+<style>
+.v-main {
+  height: calc(100vh - 56px);
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="bg-red-lighten-5 rounded-lg search-box">
+    <v-row>
+      <v-col cols="8">
+        <div class="d-flex flex-column">
+          <span class="text-h5 font-weight-bold text-red-darken-1">Find a Pokemon</span>
+          <span class="text-body-2 text-medium-emphasis">
+            Bienvenue sur la page du Pokédex entièrement dédié à Pokémon GO. Vous pouvez retrouver
+            l'ensemble des informations des Pokémon présents dans Pokémon
+          </span>
+          <v-text-field
+            class="mt-6 bg-grey-lighten-5"
+            label="Search"
+            prepend-inner-icon="mdi-magnify"
+            variant="outlined"
+            hide-details
+          ></v-text-field>
+        </div>
+      </v-col>
+      <v-spacer></v-spacer>
+      <v-col cols="3" class="search-box-img-box">
+        <img src="@/assets/img/pokedex.png" width="300" class="search-box-img" />
+      </v-col>
+    </v-row>
+  </div>
+
+  <v-container> </v-container>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {}
+})
+</script>
+
+<style lang="scss" scoped>
+.search-box {
+  margin: 24px 64px;
+  padding: 24px;
+  .search-box-img-box {
+    position: relative;
+    .search-box-img {
+      position: absolute;
+      top: -50px;
+    }
+  }
+}
+</style>
